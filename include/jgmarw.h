5ab6d1898210cc2e009803f938c8954586b7fe21
Nandhu Sam <nandhusam2k01@gmail.com>
2024-05-18 00:08:53 +0530

    header created

---
+++ include/jgmarw.h
@@ -0,0 +1,15 @@
+
+#ifndef JGMARW
+#define JGMARW 100
+
+// TODO: Add C++ extern linkage for C
+
+
+
+#include <X11/Xlib.h>
+
+Window giveRenderingWindow(); // TODO: Prototype **WILL** be changed in the future
+
+
+
+#endif // JGMARW
